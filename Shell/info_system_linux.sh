951166a8ee879b78041f6219eb1019fae23b4a4b
Robin Fanchone <rofanchone@gmail.com>
2020-11-05 09:31:31 +0100

    Ajout "Appuyez sur Entrée pour continuer" + clear pour plus de lisibilité. Signed-off-by: Robin Fanchone <rofanchone@gmail.com>

--- Shell/info_system_linux.sh
+++ Shell/info_system_linux.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+
+# Fait uniquement avec des commandes native à Debian
+
 while :
 do
 echo "
@@ -21,46 +24,68 @@ read -p "Que voulez vous savoir ? : " choix
 case "${choix}" in
 0)    echo "Voici le nom de la machine"
     hostname
+	read -p "Appuyez sur Entrée pour continuer"
+	clear
     ;;
 	
 1)    echo "Voici la Configuration IP général"
     ip a
+	read -p "Appuyez sur Entrée pour continuer"
+	clear
     ;;
 	
 2)    echo "Voici les Adresses DNS"
     cat /etc/resolv.conf
+	read -p "Appuyez sur Entrée pour continuer"
+	clear
     ;;
 	
 3)    echo "Voici l'adresse du DHCP"
     cat /var/lib/dhcp/dhclient.* | grep dhcp-server-identifier | tail -1
+	read -p "Appuyez sur Entrée pour continuer"
+	clear
     ;;
 	
 4)    echo "Voici le Modèle de la machine"
     dmidecode | grep "System Information" -A 2
+	read -p "Appuyez sur Entrée pour continuer"
+	clear
     ;;
 	
 5)    echo "Voici la version du BIOS"
     biosdecode | more
+	read -p "Appuyez sur Entrée pour continuer"
+	clear
     ;;
 
 6)    echo "Voici les Infos CPU"
 	cat /proc/cpuinfo | more
+	read -p "Appuyez sur Entrée pour continuer"
+	clear
     ;;
 	
 7)    echo "Voici les Infos RAM"
     free -m
 	cat /proc/meminfo | more
+	read -p "Appuyez sur Entrée pour continuer"
+	clear
     ;;
 8)    echo "Voici la Liste des cartes PCI"
      lspci -v | more
+	 read -p "Appuyez sur Entrée pour continuer"
+	 clear
     ;;
 	
 9)    echo "Voici les Infos Disques Dur"
      /sbin/fdisk -l | more
+	 read -p "Appuyez sur Entrée pour continuer"
+	 clear
     ;;
 
 10)    echo "Voici la Liste des Modules chargés"
     /sbin/lsmod | more
+	read -p "Appuyez sur Entrée pour continuer"
+	clear
     ;;
 
 
